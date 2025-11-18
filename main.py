f35f76b7473d98a7896fbea6a1277af9cf5c0ac4
uchechukwu090 <125163532+uchechukwu090@users.noreply.github.com>
2025-11-18 01:19:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -34,4 +34,4 @@ def get_news():
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 10000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
+    uvicorn main:app --host 0.0.0.0 --port $PORT
